e4d199f6da7d448fbcd7f5f2f8b78bd9a6b25a82
muhammad <muhammadhaziqhabibkhan76@gmail.com>
2024-01-09 18:19:09 +0500

    firstJS

---
+++ first.js
@@ -0,0 +1,40 @@
+console.log("WellCome to PJS");
+console.log("Hello World");
+
+//TWO TYPES DATA TYPE 
+
+myName = "abdullah" ;
+age = 15;
+a ="null";
+b ="undifined";
+myCnic = true ;
+console.log(typeof(myName));
+console.log(typeof(age));
+console.log(typeof('a'));
+console.log(typeof('b'));
+console.log(typeof(myCnic));
+console.table(myName , age , a , b , myCnic);
+
+// three types of decleration
+// var
+// let
+// const
+
+var city = "KARACHI";
+ console.log(city);
+city = "ISLAMABAD";
+ console.log(city);
+var city = "LAHORE";
+ console.log(city);
+
+// let Example
+ let Myage = 18;
+ console.log(Myage);
+ Myage = 20;
+ console.log(Myage);
+
+//  const
+
+const pie =3.1416;
+console.log(pie);
+// pie = 4.25;
