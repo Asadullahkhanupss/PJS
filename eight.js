5d1e73ebe5ed340f668c485c4235bee7db268cee
asp <asp@aptech.com>
2024-01-20 17:03:21 +0500

    strings

--- eight.js
+++ eight.js
@@ -4,7 +4,8 @@ console.log(str);
 console.log(str.length);
 console.log(str.includes("O"));
 console.log(str.endsWith("e"));
-
+console.log(str.replace("Upon","Apple"));
+console.log(str.toLocaleLowerCase);
 console.log(str[5]);
 
 
