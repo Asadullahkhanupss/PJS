bd1f2976989157106b35a0afe31825d756a22f9b
asp <asp@aptech.com>
2024-01-23 18:58:25 +0500

    do1m

--- dom.js
+++ dom.js
@@ -17,4 +17,20 @@ elem.className = "first";
 // elem.id = "wow";
 
 
-let Mydiv
+let Mydiv = document.getElementsByClassName('container');
+console.log(Mydiv);
+
+Mydiv[0].innerHTML = "The Quik Brown fox";
+Mydiv[0].style.color = "red";
+Mydiv[0].style.backgroundColor = "whitesmoke";
+Mydiv[0].style.fontSize = "35px";
+Mydiv[0].id = "newid";
+
+let newElem = document.createElement('p');
+Mydiv[0].appendChild(newElem);
+
+newElem.innerHTML = "this is p tag" ;
+
+let newtag =document.createElement('img');
+newElem[0].appendChild(newtag);
+newtag.src = "https://platinumlist.net/guide/wp-content/uploads/2023/03/8359_img_worlds_of_adventure-big1613913137.jpg-1024x683.webp";
