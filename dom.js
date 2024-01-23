bf3481660b7fc2484e447839b9fa81ebfbbfa42e
asp <asp@aptech.com>
2024-01-23 18:32:16 +0500

    dom

---
+++ dom.js
@@ -0,0 +1,20 @@
+// DOM > * document object model
+
+let elem = document.getElementById('one');
+
+elem.innerHTML = "Once Upon A Time";
+elem.style.backgroundColor = "red" ;
+elem.style.color = "yellow" ;
+elem.style.textAlign = "center" ;
+elem.style.padding = "12px" ;
+elem.style.fontSize = "30px" ;
+
+elem.classList.add('two');
+elem.classList.remove('two');
+
+elem.className = "first";
+
+// elem.id = "wow";
+
+
+let Mydiv
